5088406878b2442f5d6a93a890744af26b50c033
David Poslt <david.poslt@gmail.com>
2019-09-17 18:57:44 +0200

    add requirements

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+__author__ = "viperDavid"
+
+import requests
+
+web = requests.get('http://www.google.com')
+
+print(web.content)
+
+
+print("status code is {}".format(web.status_code))
