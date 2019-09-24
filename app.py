f4e636459962f397adfc62daeebdead301d9c449
David Poslt <david.poslt@gmail.com>
2019-09-24 20:53:59 +0200

    fix description

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ __author__ = "viperDavid"
 import requests
 from bs4 import BeautifulSoup
 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 web = requests.get('https://www.johnlewis.com/john-lewis-partners-warner-faux-leather-office-chair/p1891692', verify=False)
 content = web.content
 
 soup = BeautifulSoup(content, "html.parser")
-priceElement = soup.find("p",{"class":"price price--large"})
 descriptionElement = soup.find("h1",{"class":"product-header__title"})
+priceElement = soup.find("p",{"class":"price price--large"})
 
 
 # https://www.johnlewis.com/john-lewis-partners-warner-faux-leather-office-chair/p1891692
 # <p class="price price--large">£150.00</p>
 # <h1 class="product-header__title">John Lewis &amp; Partners Warner Faux Leather Office Chair</h1>
 if (web.status_code) == 200:
-    print("{}:{}".format(descriptionElement.txt.strip(),priceElement.text.strip()))
+    print("{} : {}".format(descriptionElement.text.strip(), priceElement.text.strip()))
+
 else:
     print("status code is {}".format(web.status_code))
